c29cf11025eaff2af092285faa901da39febe55e
sajasmohammmed <sajasam97@gmail.com>
2021-10-27 07:52:51 +0530

    local notification

--- lib/controller/theme_controller.dart
+++ lib/controller/theme_controller.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
+// import 'package:get_storage/get_storage.dart';
 
 class ThemeController extends GetxController {
-  final _box = GetStorage();
-  final _key = "isDarkMode";
+  // final _box = GetStorage();
+  // final _key = "isDarkMode";
+  bool _isDarkMode = true;
 
-  bool _loadThemeFromBox() => _box.read(_key) ?? false;
-  _saveThemeToBox(bool isDarkMode) => _box.write(_key, isDarkMode);
-  ThemeMode get theme => _loadThemeFromBox() ? ThemeMode.dark : ThemeMode.light;
+  // bool _loadThemeFromBox() => _box.read(_key) ?? false;
+  // _saveThemeToBox(bool isDarkMode) => _box.write(_key, isDarkMode);
+  ThemeMode get theme => _isDarkMode ? ThemeMode.dark : ThemeMode.light;
 
-  void swichTheme() {
-    Get.changeThemeMode(_loadThemeFromBox() ? ThemeMode.dark : ThemeMode.light);
-    _saveThemeToBox(!_loadThemeFromBox());
+  void swichTheme() { 
+    Get.changeThemeMode(_isDarkMode ? ThemeMode.light:ThemeMode.dark );
+    // _saveThemeToBox(!_loadThemeFromBox());
+    _isDarkMode = !_isDarkMode;
   }
 }
--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:sqflite_todo_app/controller/notification_controller.dart';
 import 'package:sqflite_todo_app/controller/theme_controller.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   final ThemeController _themeController = Get.put(ThemeController());
+  var notifyController;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    notifyController = NotifyController();
+    notifyController.initializeNotification();
+    notifyController.requestIOSPermissions();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +27,13 @@ class HomeScreen extends StatelessWidget {
         leading: GestureDetector(
           onTap: () {
             _themeController.swichTheme();
+            notifyController.displayNotification(
+                title: 'Theme Chaged',
+                body: Get.isDarkMode
+                    ? "Activated Dark Theme"
+                    : "Activated Light Theme");
+
+            notifyController.scheduledNotification();
           },
           child: Icon(Icons.mode_night),
         ),
