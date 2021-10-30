d542366eb58e09f4e33450051b6c7af6dacae672
sajasmohammmed <sajasam97@gmail.com>
2021-10-30 08:28:08 +0530

    task and date bar

--- lib/app/theme.dart
+++ lib/app/theme.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../constents.dart';
 
@@ -10,5 +12,24 @@ class Themes {
   static final dark = ThemeData(
     primaryColor: darkGreyClr,
     brightness: Brightness.dark,
+  );
+}
+
+TextStyle get subHeadingStyle {
+  return GoogleFonts.lato(
+    textStyle: TextStyle(
+      color: Get.isDarkMode?Colors.grey[400]:Colors.grey,
+      fontSize: 24.0,
+      fontWeight: FontWeight.bold,
+    )
+  );
+}
+
+TextStyle get headingStyle {
+  return GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 30.0,
+      fontWeight: FontWeight.bold,
+    )
   );
 }
--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,5 +1,9 @@
+import 'package:date_picker_timeline/date_picker_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:sqflite_todo_app/app/theme.dart';
+import 'package:sqflite_todo_app/constents.dart';
 import 'package:sqflite_todo_app/controller/notification_controller.dart';
 import 'package:sqflite_todo_app/controller/theme_controller.dart';
 
@@ -11,6 +15,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final ThemeController _themeController = Get.put(ThemeController());
   var notifyController;
+  DateTime _selectedDate = DateTime.now();
   @override
   void initState() {
     // TODO: implement initState
@@ -38,9 +43,66 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Icon(Icons.mode_night),
         ),
       ),
-      body: Center(
-        child: Text("Welcome to"),
+      body: Container(
+        margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+        child: Column(
+          children: [
+            _addtaskBar(),
+            SizedBox(
+              height: 20,
+            ),
+            _addDateBar()
+          ],
+        ),
       ),
+    );
+  }
+
+  DatePicker _addDateBar() {
+    return DatePicker(
+      DateTime.now(),
+      selectionColor: primaryClr,
+      selectedTextColor: whiteClr,
+      height: 100,
+      width: 80,
+      initialSelectedDate: DateTime.now(),
+      onDateChange: (date) {
+        _selectedDate = date;
+      },
+    );
+  }
+
+  _addtaskBar() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              DateFormat.yMMMd().format(DateTime.now()),
+              style: subHeadingStyle,
+            ),
+            Text(
+              "Today",
+              style: headingStyle,
+            )
+          ],
+        ),
+        SizedBox(
+          height: 40,
+          child: ElevatedButton(
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(primaryClr)),
+            onPressed: null,
+            child: TextButton.icon(
+              onPressed: null,
+              icon: Icon(Icons.add),
+              label: Text('Add Task'),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
